e2f1d1aa1cc24af271928bdba30fe338ad9a68af
BoscoMurray <rossamurray@gmail.com>
2017-07-25 16:42:17 +0100

    can remove dinosaurs of type

--- park.js
+++ park.js
@@ -6,5 +6,14 @@ Park.prototype.addDinosaur = function(dinosaur) {
   this.enclosure.push(dinosaur);
 }
 
+Park.prototype.removeType = function(type) {
+  var tempArray = [];
+  for (var i = 0; i < this.enclosure.length; i++) {
+    if (this.enclosure[i].type !== type) {
+      tempArray.push( this.enclosure[i] );
+    }
+  }
+  this.enclosure = tempArray;
+}
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,12 +4,14 @@ var Dinosaur = require( "../dinosaur.js" );
 
 describe("Park", function() {
   var park;
-  var trex;
+  var trex1;
+  var trex2;
   var raptor;
 
   beforeEach(function() {
     park = new Park();
-    trex = new Dinosaur( "trex", 1 );
+    trex1 = new Dinosaur( "trex", 1 );
+    trex2 = new Dinosaur( "trex", 1 );
     raptor = new Dinosaur( "raptor", 3 );
   })
 
@@ -18,7 +20,15 @@ describe("Park", function() {
   })
 
   it("park can add dinosaur to enclosure", function() {
-    park.addDinosaur(trex);
+    park.addDinosaur(trex1);
+    assert.strictEqual( park.enclosure.length, 1 );
+  })
+
+  it("can remove all of a type of dinosaur", function() {
+    park.addDinosaur(trex1);
+    park.addDinosaur(trex2);
+    park.addDinosaur(raptor);
+    park.removeType("trex");
     assert.strictEqual( park.enclosure.length, 1 );
   })
 
