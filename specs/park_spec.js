108050e2b8d419d1dd488316b434a25cb8433c48
BoscoMurray <rossamurray@gmail.com>
2017-07-25 16:23:09 +0100

    can add dinosaur to park enclosure

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,13 +4,22 @@ var Dinosaur = require( "../dinosaur.js" );
 
 describe("Park", function() {
   var park;
+  var trex;
+  var raptor;
 
   beforeEach(function() {
     park = new Park();
+    trex = new Dinosaur( "trex", 1 );
+    raptor = new Dinosaur( "raptor", 3 );
   })
 
   it("park enclosure starts empty", function() {
     assert.strictEqual( park.enclosure.length, 0 );
+  })
+
+  it("park can add dinosaur to enclosure", function() {
+    park.addDinosaur(trex);
+    assert.strictEqual( park.enclosure.length, 1 );
   })
 
 })
