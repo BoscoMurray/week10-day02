c64e05b503c991b8831505e9d5abe6e4207c4097
BoscoMurray <rossamurray@gmail.com>
2017-07-25 16:10:05 +0100

    can make dinosaur

---
+++ specs/dinosaur_spec.js
@@ -0,0 +1,20 @@
+var assert = require( "assert" );
+var Dinosaur = require( "../dinosaur.js" );
+
+describe("Dinosaur", function() {
+  var trex;
+
+  beforeEach(function() {
+    trex = new Dinosaur( "trex", 1 );
+  })
+
+  it("can set type", function() {
+    assert.strictEqual( trex.type, "trex" );
+  })
+
+  it("can set offspring per year", function() {
+    assert.strictEqual( trex.offspring, 1 );
+  })
+
+
+})
